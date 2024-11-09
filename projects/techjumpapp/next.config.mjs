03a537690a91351bcfef1c59741cdaf8b5cc13b3
Uladzimir Paliakou <uladzimir.paliakou@schibsted.com>
2024-11-09 21:52:59 +0100

    fix: next build

--- projects/techjumpapp/next.config.mjs
+++ projects/techjumpapp/next.config.mjs
@@ -1,19 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
-    redirects: async () => [
-        {
-            destination: process.env.BACKEND_ONE_URL,
-            source: '/api/backend-one',
-            permanent: true,
-
-        },
-        {
-            destination: process.env.BACKEND_TWO_URL,
-            source: '/api/backend-two',
-            permanent: true,
-        }
-    ]
+    // redirects: async () => [
+    //     {
+    //         destination: process.env.BACKEND_ONE_URL,
+    //         source: '/api/backend-one',
+    //         permanent: true,
+    //
+    //     },
+    //     {
+    //         destination: process.env.BACKEND_TWO_URL,
+    //         source: '/api/backend-two',
+    //         permanent: true,
+    //     }
+    // ]
 };
 
 export default nextConfig;
