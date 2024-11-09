c50f8836f2237b9c31565a7786102eb8c593ca60
Uladzimir Paliakou <uladzimir.paliakou@schibsted.com>
2024-11-10 00:27:33 +0100

    feat: checking how cache works

--- packages/ui-kit/blocker.js
+++ packages/ui-kit/blocker.js
@@ -1,6 +1,6 @@
 const foo = () => {
     return new Promise(resolve => setTimeout(()=> {
-        resolve('we are done! Let try another change!')
+        resolve('we are done! Let try another change!111')
     }, 5000));
 }
 
