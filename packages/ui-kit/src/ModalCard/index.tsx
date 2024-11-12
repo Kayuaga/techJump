e2113946db9b375a813fe6f3498afc7281b5275d
Uladzimir Paliakou <uladzimir.paliakou@schibsted.com>
2024-11-12 20:57:34 +0100

    chore: changed version

--- packages/ui-kit/src/ModalCard/index.tsx
+++ packages/ui-kit/src/ModalCard/index.tsx
@@ -16,7 +16,7 @@ export const ModalCard: FC<IProps> = ({isThin, header, children}) => {
             className={styles.container}>
             <div className={styles.headerContainer}>
                 <div className={styles.header}>{header}</div>
-                <button onClick={()=> console.log('HELLO!v1.1.0')}>Cross icon should be there=)</button>
+                <button onClick={()=> console.log('HELLO!v1.1.1')}>Cross icon should be there=)</button>
             </div>
             <>{children}</>
         </div>
