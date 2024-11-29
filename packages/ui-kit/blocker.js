0beb8446c4def47d8104cf64299bea275f066050
Uladzimir Paliakou <uladzimir.paliakou@schibsted.com>
2024-11-29 13:24:54 +0100

    chore: testing cache again!

--- packages/ui-kit/blocker.js
+++ packages/ui-kit/blocker.js
@@ -1,6 +1,6 @@
 const foo = () => {
     return new Promise(resolve => setTimeout(()=> {
-        resolve('we are done!Lets see how it works with with cashe from master branch! Lets test it')
+        resolve('we are done!Lets see how it works with with cashe from master branch! Lets test it again')
     }, 5000));
 }
 
