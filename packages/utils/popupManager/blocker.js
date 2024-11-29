00a21e0e98634bd932fee2f1b05d112c9a4f9a18
Uladzimir Paliakou <uladzimir.paliakou@schibsted.com>
2024-11-29 15:12:21 +0100

    chore: testing concurrency again

--- packages/utils/popupManager/blocker.js
+++ packages/utils/popupManager/blocker.js
@@ -1,6 +1,6 @@
 const foo = () => {
     return new Promise(resolve => setTimeout(()=> {
-        resolve('we are done! lets see here!!!!!!1111 and the final one')
+        resolve('we are done! lets see here!!!!!!11 and the final one')
     }, 5000));
 }
 
