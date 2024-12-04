644b7d4b728348b62e1cdb6410508de06e6ad2ea
Uladzimir Paliakou <uladzimir.paliakou@schibsted.com>
2024-12-04 22:02:00 +0100

    feat: added docker build and push

--- projects/cdkInfra/bin/cdk_infra.ts
+++ projects/cdkInfra/bin/cdk_infra.ts
@@ -5,6 +5,8 @@ import {EcrInfraStack} from '../lib/cdk_infra-stack';
 const app = new cdk.App();
 
 new EcrInfraStack(app, process.env.ECR_ID as string, {
-    repositoryName: `${process.env.REPOSITORY_NAME}_${process.env.PROJECT_NAME}`,
+    repositoryName: `${process.env.REPOSITORY_NAME}-${process.env.PROJECT_NAME}`,
     repoId: process.env.REPOSITORY_ID as string,
 });
+
+//split for each project
