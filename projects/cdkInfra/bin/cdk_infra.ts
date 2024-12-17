70e1ed509553c06810dc997d0651f8467d2053d4
Uladzimir Paliakou <uladzimir.paliakou@schibsted.com>
2024-12-17 22:07:07 +0100

    feat: lets try and deploy it =)

--- projects/cdkInfra/bin/cdk_infra.ts
+++ projects/cdkInfra/bin/cdk_infra.ts
@@ -5,21 +5,21 @@ import {MyEksClusterStack} from "../lib/eks_Cluster";
 
 const app = new cdk.App();
 
-// new EcrInfraStack(app, process.env.ECR_ID as string, {
-//     repositoryName: `${process.env.REPOSITORY_NAME}-${process.env.PROJECT_NAME}`,
-//     repoId: process.env.REPOSITORY_ID as string,
-// });
-
-new MyEksClusterStack(app, 'MyEksClusterStack', {
-    clusterName: 'my-eks-cluster',
-    clusterVersion: cdk.aws_eks.KubernetesVersion.V1_31,
-    vpcCidr: '10.0.0.0/16',
-    amiReleaseVersion: 'latest',
-    tags: {
-        'Environment': 'Dev',
-        'Owner': 'Test',
-    },
+new EcrInfraStack(app, process.env.ECR_ID as string, {
+    repositoryName: `${process.env.REPOSITORY_NAME}-${process.env.PROJECT_NAME}`,
+    repoId: process.env.REPOSITORY_ID as string,
 });
+
+// new MyEksClusterStack(app, 'MyEksClusterStack', {
+//     clusterName: 'my-eks-cluster',
+//     clusterVersion: cdk.aws_eks.KubernetesVersion.V1_31,
+//     vpcCidr: '10.0.0.0/16',
+//     amiReleaseVersion: 'latest',
+//     tags: {
+//         'Environment': 'Dev',
+//         'Owner': 'Test',
+//     },
+// });
 
 
 app.synth();
