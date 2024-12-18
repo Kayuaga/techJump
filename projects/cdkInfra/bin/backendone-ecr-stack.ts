535d74f6fae7a9bfaf327fc30eadd0bc81859e44
Uladzimir Paliakou <uladzimir.paliakou@schibsted.com>
2024-12-18 13:57:47 +0100

    fix: small fix again

--- projects/cdkInfra/bin/backendone-ecr-stack.ts
+++ projects/cdkInfra/bin/backendone-ecr-stack.ts
@@ -10,28 +10,6 @@ new EcrInfraStack(app, 'backendone', {
     repoId: process.env.REPOSITORY_ID as string,
 });
 
-new EcrInfraStack(app, 'backendtwo', {
-    repositoryName: `${process.env.REPOSITORY_NAME}-${process.env.PROJECT_NAME}`,
-    repoId: process.env.REPOSITORY_ID as string,
-});
-
-new EcrInfraStack(app, 'techjumpapp', {
-    repositoryName: `${process.env.REPOSITORY_NAME}-${process.env.PROJECT_NAME}`,
-    repoId: process.env.REPOSITORY_ID as string,
-});
-
-new MyEksClusterStack(app, 'MyEksClusterStack', {
-    clusterName: 'my-eks-cluster',
-    clusterVersion: cdk.aws_eks.KubernetesVersion.V1_31,
-    vpcCidr: '10.0.0.0/16',
-    amiReleaseVersion: 'latest',
-    tags: {
-        'Environment': 'Dev',
-        'Owner': 'Test',
-    },
-});
-
-
 app.synth();
 
 //split for each project
