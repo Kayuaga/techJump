15502536ee63aee0305b8f01cc32446c96ac43e6
Uladzimir Paliakou <to.polyakov.vova@gmail.com>
2025-01-02 17:11:44 +0100

    Deploying to kube cluster
    Prepared workflows that deploys PR to the EKS cluster

--- projects/backendone/app.js
+++ projects/backendone/app.js
@@ -2,10 +2,10 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = process.env.PORT
-console.log(port, 'PORT 1')
+console.log(port, 'PORT 1 and other namespace')
 
 app.get('/', (req, res) => {
-    res.json({ hello:'Hello World! backend 1!' })
+    res.json({ hello:'Hello World! backend 1! and other name space' })
 })
 
 app.listen(port, () => {
--- projects/cdkInfra/bin/techjumpapp-ecr-stack.ts
+++ projects/cdkInfra/bin/techjumpapp-ecr-stack.ts
@@ -4,9 +4,13 @@ import {EcrInfraStack} from '../lib/cdk_infra-stack';
 
 const app = new cdk.App();
 
-new EcrInfraStack(app, process.env.ECR_ID as string, {
+
+new EcrInfraStack(app, `techjumpapp-${process.env.REPOSITORY_NAME}-${process.env.PROJECT_NAME}`, {
     repositoryName: `${process.env.REPOSITORY_NAME}-${process.env.PROJECT_NAME}`,
     repoId: process.env.REPOSITORY_ID as string,
 });
+
+
+app.synth();
 
 //split for each project
