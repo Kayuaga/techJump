ad7ae0ff4e38686b1d0fa0a08de1b9592197fe2f
Uladzimir Paliakou <uladzimir.paliakou@schibsted.com>
2025-01-12 21:29:13 +0100

    feat: welcome to helm charts

---
+++ infra/scripts/patchHelmConfigMap.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+required_vars=("CONFIG_MAP_NAME" "VARIABLE_NAME" "VALUE" "NAME_SPACE")
+for var in "${required_vars[@]}"; do
+  if [[ -z "${!var}" ]]; then
+    echo "Error: Environment variable $var is not set."
+    exit 1
+  fi
+done
+
+echo "Patching config map..."
+
+helm upgrade "$CONFIG_MAP_NAME" ./configmap-helm --set env."$VARIABLE_NAME"="$VALUE" --namespace "$NAME_SPACE" --create-namespace
