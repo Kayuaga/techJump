86d3a809b57c5b805550cf0896a2251a35c47c3e
Uladzimir Paliakou <uladzimir.paliakou@schibsted.com>
2025-01-14 18:04:29 +0100

    feat: reusable helm charts

--- packages/ui-kit/blocker.js
+++ packages/ui-kit/blocker.js
@@ -1,6 +1,6 @@
 const foo = () => {
     return new Promise(resolve => setTimeout(()=> {
-        resolve('we are done!Lets see how it works with with cashe from master branch!' +
+        resolve('we are done!Lets see how it works with with cache from master branch!' +
             ' Lets test it again testing cuncurrency')
     }, 5000));
 }
